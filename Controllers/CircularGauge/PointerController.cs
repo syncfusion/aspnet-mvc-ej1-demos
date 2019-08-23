9bca9c1cba018828cc70d3bc0fe9ade7fe4258bd
SyncfusionInstall <buildautomation@syncfusion.com>
2019-08-23 14:56:00 +0530

    v17.2.0.46

---
+++ Controllers/CircularGauge/PointerController.cs
@@ -0,0 +1,33 @@
+#region Copyright Syncfusion Inc. 2001-2019.
+// Copyright Syncfusion Inc. 2001-2019. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using MVCSampleBrowser.Models.CircularGauge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSampleBrowser.Controllers.CircularGauge
+{
+    public partial class CircularGaugeController : Controller
+    {
+        //
+        // GET: /Pointer/
+        List<NeedleStyle> needleStyle = new List<NeedleStyle>();
+        public ActionResult Pointer()
+        {
+            needleStyle.Add(new NeedleStyle { Style = "triangle" });
+            needleStyle.Add(new NeedleStyle { Style = "trapezoid" });
+            needleStyle.Add(new NeedleStyle { Style = "arrow" });
+            needleStyle.Add(new NeedleStyle { Style = "rectangle" });
+            ViewBag.datasource = needleStyle;
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/Ribbon/ResizeController.cs
@@ -0,0 +1,41 @@
+#region Copyright Syncfusion Inc. 2001-2019.
+// Copyright Syncfusion Inc. 2001-2019. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using MVCSampleBrowser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSampleBrowser.Controllers
+{
+    public partial class RibbonController : Controller
+    {
+        //
+        // GET: /Resize/
+        List<FontFamily> fontFamily6 = new List<FontFamily>();
+        List<FontPoint> fontPoint6 = new List<FontPoint>();
+        public ActionResult Resize()
+        {
+              fontFamily6.Add(new FontFamily { text = "Segoe UI" });
+              fontFamily6.Add(new FontFamily { text = "Arial" });
+              fontFamily6.Add(new FontFamily { text = "Times New Roman" });
+              fontFamily6.Add(new FontFamily { text = "Tahoma" });
+              fontFamily6.Add(new FontFamily { text = "Helvetica" });
+            ViewBag.datasource =   fontFamily6;
+           fontPoint6.Add(new FontPoint { text = "1pt" });
+           fontPoint6.Add(new FontPoint { text = "2pt" });
+           fontPoint6.Add(new FontPoint { text = "3pt" });
+           fontPoint6.Add(new FontPoint { text = "4pt" });
+           fontPoint6.Add(new FontPoint { text = "5pt" });
+            ViewBag.datasource1 =fontPoint6;
+            return View();
+        }
+
+    }
+}
