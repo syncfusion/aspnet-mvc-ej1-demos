6f0a88d42231ac5ffec379c9cfe8d58ea158d153
SyncfusionInstall <buildautomation@syncfusion.com>
2020-12-17 16:20:59 +0530

    v18.4.0.30

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("18.3.0.47")]
+[assembly: AssemblyVersion("18.4.0.30")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
