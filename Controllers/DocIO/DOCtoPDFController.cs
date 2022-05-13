61c1cec8be67e44e406df22fe50dca7c316e3e87
SyncfusionBuild <buildautomation@syncfusion.com>
2022-05-13 11:55:30 +0530

    v20.1.0.55

---
+++ Controllers/DocIO/DOCtoPDFController.cs
@@ -0,0 +1,90 @@
+#region Copyright
+// Copyright Syncfusion Inc. 2001-2022. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using Syncfusion.DocIO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocToPDFConverter;
+using Syncfusion.Pdf;
+using Syncfusion.Mvc.Pdf;
+using Syncfusion.OfficeChartToImageConverter;
+
+namespace MvcSampleBrowser_2012.Controllers
+{
+    public partial class DocIOController : Controller
+    {
+        #region doc to PDF
+        public ActionResult DOCtoPDF(string button, string renderingMode, string renderingMode1, string renderingMode2, string renderingMode3, string renderingMode4, string renderingMode5, string renderingMode6, HttpPostedFileBase file)
+        {
+            if (button == null)
+                return View();
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLower();
+                if (extension == ".doc" || extension == ".docx" || extension == ".dot" || extension == ".dotx" || extension == ".dotm" || extension == ".docm"
+                   || extension == ".xml"  || extension == ".rtf")
+                {
+                    WordDocument document = new WordDocument(file.InputStream);
+
+                    //Initialize chart to image converter for converting charts in Word to PDF conversion
+                    document.ChartToImageConverter = new ChartToImageConverter();
+                    document.ChartToImageConverter.ScalingMode = Syncfusion.OfficeChart.ScalingMode.Normal;
+
+                    DocToPDFConverter converter = new DocToPDFConverter();
+                    //Enable Direct PDF rendering mode for faster conversion.
+                    if (renderingMode == "DirectPDF")
+                        converter.Settings.EnableFastRendering = true;
+                    if (renderingMode1 == "PreserveStructureTags")
+                        converter.Settings.AutoTag = true;
+                    if (renderingMode2 == "PreserveFormFields")
+                        converter.Settings.PreserveFormFields = true;
+                    converter.Settings.ExportBookmarks = renderingMode3 == "PreserveWordHeadingsToPDFBookmarks"
+                                      					 ? Syncfusion.DocIO.ExportBookmarkType.Headings
+														 : Syncfusion.DocIO.ExportBookmarkType.Bookmarks;
+                    if (renderingMode4 == "EnablesCompleteFont")
+                        converter.Settings.EmbedCompleteFonts = true;
+                    if (renderingMode5 == "EnablesSubsetFont")
+                        converter.Settings.EmbedFonts = true;
+                    if(renderingMode6 == "ShowRevisions")
+                        document.RevisionOptions.ShowMarkup = RevisionType.Deletions | RevisionType.Formatting | RevisionType.Insertions;
+                    //Convert word document into PDF document
+                    PdfDocument pdfDoc = converter.ConvertToPDF(document);
+                    try
+                    {
+                        return pdfDoc.ExportAsActionResult("sample.pdf", HttpContext.ApplicationInstance.Response, HttpReadType.Save);
+                    }
+                    catch (Exception)
+                    { }
+                    finally
+                    {
+
+                    }
+                }
+                else
+                {
+                    ViewBag.Message = string.Format("Please choose Word format document to convert to PDF");
+                }
+            }
+            else
+            {
+                ViewBag.Message = string.Format("Browse a Word document and then click the button to convert as a PDF document");
+            }
+
+            return View();
+        }
+
+        #endregion doc to PDF
+    }
+}
---
+++ Controllers/PdfViewer/DefaultController.cs
@@ -0,0 +1,113 @@
+#region Copyright Syncfusion Inc. 2001-2022.
+// Copyright Syncfusion Inc. 2001-2022. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Syncfusion.EJ.PdfViewer;
+using System.IO;
+using System.Reflection;
+
+namespace MVCSampleBrowser.Controllers
+{
+    public partial class PdfViewerController : Controller
+    {
+        //
+        // GET: /PdfViewerDefault/
+
+        public ActionResult Default()
+        {
+            return View();
+        }
+		        [HttpPost]
+        public ActionResult Load(jsonObjects results)
+        {
+            PdfViewerHelper helper = new PdfViewerHelper();
+            var jsonResult = JsonConverterstring(results);
+			//load the multiple document from client side 
+            if (jsonResult.ContainsKey("newFileName"))
+            {
+                var name = jsonResult["newFileName"];
+                var pdfName = name.ToString() + ".pdf";
+                helper.Load(Helper.GetFilePath("" + pdfName));
+            }
+            else
+            {
+			//Initially load the PDF document from the data folder.
+                if (jsonResult.ContainsKey("isInitialLoading"))
+                {
+                    helper.Load(Helper.GetFilePath("FormFillingDocument.pdf"));
+                }
+            }
+            return Content(JsonConvert.SerializeObject(helper.ProcessPdf(jsonResult)));
+        }
+        public Dictionary<string, string> JsonConverterstring(jsonObjects results)
+        {
+            Dictionary<string, object> resultObjects = new Dictionary<string, object>();
+            resultObjects = results.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .ToDictionary(prop => prop.Name, prop => prop.GetValue(results, null));
+            var emptyObjects = (from kv in resultObjects
+                                where kv.Value != null
+                                select kv).ToDictionary(kv => kv.Key, kv => kv.Value);
+            Dictionary<string, string> jsonResult = emptyObjects.ToDictionary(k => k.Key, k => k.Value.ToString());
+            return jsonResult;
+        }
+        public JsonResult Download(jsonObjects result)
+        {
+            PdfViewerHelper helper = new PdfViewerHelper();
+            var jsonResult = JsonConverterstring(result);
+            if (jsonResult.ContainsKey("savedFields"))
+            {
+                var values = jsonResult["savedFields"];
+                Dictionary<string, string> data = JsonConvert.DeserializeObject<Dictionary<string, string>>(values);
+                jsonResult = data;
+            }
+            return Json(helper.GetDocumentData(jsonResult));
+        }
+        public ActionResult FileUpload(jsonObjects result)
+        {
+            PdfViewerHelper helper = new PdfViewerHelper();
+            var jsonResult = JsonConverterstring(result);
+            if (jsonResult.ContainsKey("uploadedFile"))
+            {
+                var fileurl = jsonResult["uploadedFile"];
+                byte[] byteArray = Convert.FromBase64String(fileurl);
+                MemoryStream stream = new MemoryStream(byteArray);
+                helper.Load(stream);
+            }
+            return Content(JsonConvert.SerializeObject(helper.ProcessPdf(jsonResult)));
+        }
+
+    }
+	    public class jsonObjects
+    {
+        public string viewerAction { get; set; }
+        public string pageindex { get; set; }
+        public string controlId { get; set; }
+        public string isInitialLoading { get; set; }
+        public string id { get; set; }
+        public string isPageScrolled { get; set; }
+        public string Download { get; set; }
+        public string uploadedFile { get; set; }
+        public string newFileName { get; set; }
+        public string savedFields { get; set; }
+        public string enableOfflineMode { get; set; }
+    }
+    public class Helper
+    {
+        public static string GetFilePath(string path)
+        {
+            string _dataPath = new System.IO.DirectoryInfo(HttpContext.Current.Request.PhysicalApplicationPath
+                + "..\\..\\..\\Common\\Data\\PDF").FullName;
+            _dataPath += @"\" + path;
+            return _dataPath;
+        }
+    }
+}
